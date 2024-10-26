4d4c57ce7639058c231841e321348b2c3a1b6fd0
Mila <andonovikjmila@gmail.com>
2024-10-27 00:05:40 +0200

    Initial commit

---
+++ src/main/java/com/example/doniraj/DonirajApplication.java
@@ -0,0 +1,13 @@
+package com.example.doniraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonirajApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonirajApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doniraj/DonirajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doniraj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonirajApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
