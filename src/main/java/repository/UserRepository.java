7c4833e6b6c4413ae43bb2767530757493340c73
Mila <andonovikjmila@gmail.com>
2024-10-31 21:20:57 +0100

    Configured Postgres connection unsuccessfully

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
